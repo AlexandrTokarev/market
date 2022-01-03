c7d07b7a3eb0d06a6c7798c773252035ccb0bb9c
Alexandr Tokarev <46864005+AlexandrTokarev@users.noreply.github.com>
2022-01-03 14:51:03 +0600

    Add Info page

--- src/app/pages/bars-page/bars-routing.module.ts
+++ src/app/pages/bars-page/bars-routing.module.ts
@@ -1,13 +1,22 @@
-import {NgModule} from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
-import {BarsPageComponent} from './bars-page.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AngularYandexMapsModule, YaConfig } from 'angular8-yandex-maps';
+import { BarsPageComponent } from './bars-page.component';
 
 const routes: Routes = [
-	{path: '', component: BarsPageComponent}
+	{ path: '', component: BarsPageComponent }
 ];
 
+const mapConfig: YaConfig = {
+	apikey: 'API_KEY',
+	lang: 'en_US',
+};
+
 @NgModule({
-	imports: [RouterModule.forChild(routes)],
+	imports: [
+		RouterModule.forChild(routes),
+		AngularYandexMapsModule.forRoot(mapConfig)
+	],
 	exports: [RouterModule],
 	declarations: [
 		BarsPageComponent
